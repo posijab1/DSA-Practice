d5261609976de9805acdbd94d53576f5ebb41b53
posijab <sebastian_klix@hotmail.com>
2022-07-02 15:53:21 -0300

    asd1

--- Maximun array.py
+++ Maximun array.py
@@ -3,13 +3,12 @@ def maxSubArraySum(arr):
     aux1 = arr[0]
     aux2 = 0
 
-    for i in range(len(arr)-1):
-        aux2 += arr[i]
-        if aux2 < 0:
-            aux2 = 0
-        elif aux1 < aux2:
-            aux1 = aux2
-    return aux1
-
+    for i in arr:
+        aux2 += i
+        if aux1 < 0:
+            aux1 = 0
+        elif aux1 > aux2:
+            aux2 = aux1
+    return aux2
 
 print(maxSubArraySum([4, -3, 4, -1, -2, 5, -3]))
